a38ca8f0a890d426134e676c5a7f3f8e1adf4d21
Youssef George <yousifkhaleel00@gmail.com>
2025-02-18 09:19:25 +0200

    add accept client and read stream functionality

--- TCPServer/Form1.cs
+++ TCPServer/Form1.cs
@@ -1,11 +1,15 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace TCPServer
 {
     public partial class Form1 : Form
     {
         TcpListener listener;
+        TcpClient tcpClient;
+        byte[] buffer;
+
         public Form1()
         {
             InitializeComponent();
@@ -27,6 +31,63 @@ namespace TCPServer
 
             listener = new TcpListener(ipAddress, port);
             listener.Start();
+
+            listener.BeginAcceptTcpClient(onCompleteAcceptTcpClient, listener);
+        }
+
+        private void onCompleteAcceptTcpClient(IAsyncResult iar)
+        { 
+            TcpListener tcp = (TcpListener)iar.AsyncState;
+            try
+            {
+                tcpClient = tcp.EndAcceptTcpClient(iar);
+                printLine("Client connected...");
+                buffer = new byte[512];
+                tcpClient.GetStream().BeginRead(buffer, 0, buffer.Length, onCompleteReadFromTcpClientStream, tcpClient);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void onCompleteReadFromTcpClientStream(IAsyncResult ar)
+        {
+            TcpClient client;
+            int readByteCount = 0;
+            string strRecv;
+
+            try
+            {
+                client = (TcpClient)ar.AsyncState;
+                readByteCount = client.GetStream().EndRead(ar);
+
+                if (readByteCount == 0)
+                {
+                    MessageBox.Show("Client disconnected");
+                    return;
+                }
+
+                strRecv = Encoding.ASCII.GetString(buffer, 0, readByteCount);
+                printLine(strRecv);
+
+                buffer = new byte[512];
+                client.GetStream().BeginRead(buffer, 0, buffer.Length, onCompleteReadFromTcpClientStream, client);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void printLine(string strLine)
+        {
+            tbConsoleOutput.Invoke(new Action<string>(doInvoke), strLine);
+        }
+
+        public void doInvoke(string strLine)
+        {
+            tbConsoleOutput.Text = strLine + Environment.NewLine + tbConsoleOutput.Text;
         }
     }
 }
