1eba6c52b9c866c7f4f918d24e09e0dd759ff4ca
Youssef George <yousifkhaleel00@gmail.com>
2025-02-18 07:55:04 +0200

    validate IP and Port to initialize TCPLisener

--- TCPServer/Form1.cs
+++ TCPServer/Form1.cs
@@ -1,7 +1,11 @@
+using System.Net;
+using System.Net.Sockets;
+
 namespace TCPServer
 {
     public partial class Form1 : Form
     {
+        TcpListener listener;
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +13,20 @@ namespace TCPServer
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            IPAddress ipAddress;
+            int port;
 
+            if (!int.TryParse(tbPortNumber.Text, out port))
+                port = 23000;
+
+            if(!IPAddress.TryParse(tbIPAddress.Text, out ipAddress))
+            {
+                MessageBox.Show("Invalid IP Address!");
+                return;
+            }
+
+            listener = new TcpListener(ipAddress, port);
+            listener.Start();
         }
     }
 }
